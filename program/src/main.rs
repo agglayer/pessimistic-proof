0c941b8beb6b8197bab387006fc380591fa06120
Philippe Laferriere <plafer@protonmail.com>
2024-04-05 13:01:17 -0400

    program: add invariant-proof dependency

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,12 +1,7 @@
-//! A simple program to be proven inside the zkVM.
-
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
 pub fn main() {
-    // NOTE: values of n larger than 186 will overflow the u128 type,
-    // resulting in output that doesn't match fibonacci sequence.
-    // However, the resulting proof will still be valid!
     let n = sp1_zkvm::io::read::<u32>();
     let mut a: u128 = 0;
     let mut b: u128 = 1;
