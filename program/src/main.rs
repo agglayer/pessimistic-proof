24bc5ef735bc7a2a8fa44a4093d2e855188aa4f6
Philippe Laferriere <plafer@protonmail.com>
2024-04-05 16:48:41 -0400

    fmt

--- program/src/main.rs
+++ program/src/main.rs
@@ -13,7 +13,8 @@ pub fn main() {
     let local_exit_root = sp1_zkvm::io::read::<KeccakDigest>();
     let withdrawals = sp1_zkvm::io::read::<Vec<Withdrawal>>();
 
-    let (new_local_exit_root, aggregate_deposits) = leaf_proof(local_exit_tree, local_exit_root, withdrawals).unwrap();
+    let (new_local_exit_root, aggregate_deposits) =
+        leaf_proof(local_exit_tree, local_exit_root, withdrawals).unwrap();
 
     sp1_zkvm::io::commit(&new_local_exit_root);
     sp1_zkvm::io::commit(&aggregate_deposits.hash());
