189b772da428d91b3fb18edb16c718ea0bc445ce
Monir Hadji <hadji.szs@gmail.com>
2024-05-09 17:03:38 -0400

    adapt the sp1 program for leaf proof

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,23 +1,14 @@
 #![no_main]
 
-use poly_pessimistic_proof::{
-    generate_leaf_proof,
-    keccak::Digest as KeccakDigest,
-    local_exit_tree::{hasher::Keccak256Hasher, LocalExitTree},
-    Withdrawal,
-};
+use poly_pessimistic_proof::{batch::Batch, generate_leaf_proof};
 
 sp1_zkvm::entrypoint!(main);
 
 pub fn main() {
-    let local_exit_tree = sp1_zkvm::io::read::<LocalExitTree<Keccak256Hasher>>();
-    let initial_local_exit_root = sp1_zkvm::io::read::<KeccakDigest>();
-    let withdrawals = sp1_zkvm::io::read::<Vec<Withdrawal>>();
+    let batch = sp1_zkvm::io::read::<Batch>();
 
-    let (new_local_exit_root, aggregate_deposits) =
-        generate_leaf_proof(local_exit_tree, initial_local_exit_root, withdrawals).unwrap();
+    let (new_local_exit_root, aggregate) = generate_leaf_proof(batch).unwrap();
 
-    sp1_zkvm::io::commit(&initial_local_exit_root);
     sp1_zkvm::io::commit(&new_local_exit_root);
-    sp1_zkvm::io::commit(&aggregate_deposits.hash());
+    sp1_zkvm::io::commit(&aggregate);
 }
